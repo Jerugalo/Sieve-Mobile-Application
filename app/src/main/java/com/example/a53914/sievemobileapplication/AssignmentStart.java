33032825c458cac8cff31bdeacc407999d2e8b05
BenHarperStudent <71878@psdschools.org>
2018-09-28 14:09:44 -0600

    Added Assignment Start and Settings
    Just what the title says :P

---
+++ app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentStart.java
@@ -0,0 +1,13 @@
+package com.example.a53914.sievemobileapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssignmentStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_start);
+    }
+}
