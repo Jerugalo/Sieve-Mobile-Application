85c2318c289925d5371a0119303a70eb411ab879
AKmeier <43421311+AKmeier@users.noreply.github.com>
2018-10-17 14:25:18 -0600

    Added "Back to Home" buttons for Settings and TaskCreate pages

--- app/src/main/java/com/example/a53914/sievemobileapplication/TaskCreate.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/TaskCreate.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TaskCreate extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TaskCreate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_create);
+    }
+    //Returning Home
+    public void toHomePage(View view){
+        Intent toHomePage = new Intent(this, HomePage.class);
+        startActivity(toHomePage);
     }
 }
