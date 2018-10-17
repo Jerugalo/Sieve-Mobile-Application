2dc7d2f11aa669cfecbdc927ffe041527d5021ad
AKmeier <43421311+AKmeier@users.noreply.github.com>
2018-10-17 13:42:40 -0600

    Added Homepage -> Task Creation

--- app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+    //The function below is called when user clicks on "+" key on the homepage.
+    public void toAssignmentCreation(View view){
+        Intent toAssignmentCreation = new Intent(this, TaskCreate.class);
+        startActivity(toAssignmentCreation);
     }
 }
