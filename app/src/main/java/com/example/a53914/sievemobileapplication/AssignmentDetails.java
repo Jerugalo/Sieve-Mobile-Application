31a50d057407ee7802c0bcb882b62c1ab48d10d3
AKmeier <43421311+AKmeier@users.noreply.github.com>
2018-10-22 13:35:47 -0600

    Completed page switching

--- app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentDetails.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentDetails.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AssignmentDetails extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AssignmentDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_details);
+    }
+    //Returning Home
+    public void toHomePage(View view){
+        Intent toHomePage = new Intent(this, HomePage.class);
+        startActivity(toHomePage);
     }
 }
--- app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentStart.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentStart.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AssignmentStart extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class AssignmentStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_start);
+    }
+
+    public void taskCancel(View view){
+        Intent taskCancel = new Intent(this, HomePage.class);
+        startActivity(taskCancel);
+    }
+    public void taskComplete(View view){
+        Intent taskComplete = new Intent(this, HomePage.class);
+        startActivity(taskComplete);
     }
 }
