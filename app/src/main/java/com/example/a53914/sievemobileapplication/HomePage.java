ccbbaf032227c77a52abe5b4b3b2617ff7eeaca5
AKmeier <43421311+AKmeier@users.noreply.github.com>
2018-10-24 13:52:57 -0600

    Manually merged the working theme into the develop branch
    hopefully it wont break everything

--- app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
     }
+    //Function called when user opens Settings//
+    public void toSettingsMenu(android.view.View view){
+        Intent toSettingsMenu = new Intent(this, Settings.class);
+        startActivity(toSettingsMenu);
+    }
+    //The function below is called when user clicks on "+" key on the homepage.
+    public void toAssignmentCreation(View view){
+        Intent toAssignmentCreation = new Intent(this, TaskCreate.class);
+        startActivity(toAssignmentCreation);
+    }
+
 }
