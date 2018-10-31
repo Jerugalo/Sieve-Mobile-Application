c8528799a2d94ec59c1dfe95d616f319c66f3c38
AKmeier <43421311+AKmeier@users.noreply.github.com>
2018-10-31 13:50:38 -0600

    Added page switching functionality to Assignment Details

--- app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentDetails.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/AssignmentDetails.java
@@ -1,7 +1,9 @@
 package com.example.a53914.sievemobileapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AssignmentDetails extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class AssignmentDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_details);
+    }
+    public void toHomePage(View view){
+        Intent toHomePage = new Intent(this, HomePage.class);
+        startActivity(toHomePage);
+    }
+    public void editTask(View view){
+        Intent editTask = new Intent (this, TaskCreate.class);
+        startActivity(editTask);
+    }
+    public void beginTask(View view){
+        Intent beginTask = new Intent(this, AssignmentStart.class);
+        startActivity(beginTask);
     }
 }
