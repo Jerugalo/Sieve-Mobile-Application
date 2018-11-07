1c95595e2855ae0ddf7cfe240e7b0cd5626b9242
HFocus <hfocus77@gmail.com>
2018-11-07 14:32:07 -0700

    TaskDao.java documentation

--- app/src/main/java/com/example/a53914/sievemobileapplication/db/TaskDao.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/db/TaskDao.java
@@ -2,15 +2,26 @@ package com.example.a53914.sievemobileapplication.db;
 
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 
+/**
+ * Defines various ways to interact with the database.
+ */
+
 @Dao
 public interface TaskDao {
     @Query("SELECT * FROM Task")
     List<Task> getAll();
+
+    @Insert
+    void insertAll(Task task);
+
+    @Delete
+    void delete(Task user);
 
     //@Query("SELECT * FROM activities WHERE uid IN (:userIds)")
     //List<Task> loadAllByIds(int[] userIds);
@@ -18,13 +29,4 @@ public interface TaskDao {
     //@Query("SELECT * FROM activities WHERE first_name LIKE :first AND "
     //        + "last_name LIKE :last LIMIT 1")
     //Task findByName(String first, String last);
-
-    @Insert
-    void insertAll(Task task);
-
-// --Commented out by Inspection START (10/31/2018 2:03 PM):
-//    @Delete
-//    void delete(Task user);
-// --Commented out by Inspection STOP (10/31/2018 2:03 PM)
-
 }
