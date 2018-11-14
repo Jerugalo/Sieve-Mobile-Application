044ad40003ec6b5976ee9a73e1782b2546b487be
HFocus <hfocus77@gmail.com>
2018-11-14 13:27:39 -0700

    Require Non-Null Values in DatePickerFragment.java

--- app/src/main/java/com/example/a53914/sievemobileapplication/fragments/DatePickerFragment.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/fragments/DatePickerFragment.java
@@ -3,6 +3,7 @@ package com.example.a53914.sievemobileapplication.fragments;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -10,9 +11,11 @@ import android.widget.TextView;
 import com.example.a53914.sievemobileapplication.R;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         final Calendar c = Calendar.getInstance();
@@ -20,12 +23,12 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int month =c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        return new DatePickerDialog(getActivity(),this,year,month,day);
+        return new DatePickerDialog(Objects.requireNonNull(getActivity()),this,year,month,day);
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        TextView datetext=(TextView) getActivity().findViewById(R.id.DateViewer);
+        TextView datetext=(TextView) Objects.requireNonNull(getActivity()).findViewById(R.id.DateViewer);
         month = month +1;
         String dateString = month + "/" + dayOfMonth + "/" +year;
         datetext.setText(dateString);
