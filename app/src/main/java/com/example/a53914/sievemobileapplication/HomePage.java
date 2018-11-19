c6fca154f2cc09de8aa1d6a37e419eb9d0d46dbf
KaitlynAlmquist <43421309+KaitlynAlmquist@users.noreply.github.com>
2018-11-19 14:49:43 -0700

    debug error

--- app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/HomePage.java
@@ -9,7 +9,7 @@ public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home_page);
         boolean useDark = getIntent().getBooleanExtra("usedark",true);
         if(useDark){
