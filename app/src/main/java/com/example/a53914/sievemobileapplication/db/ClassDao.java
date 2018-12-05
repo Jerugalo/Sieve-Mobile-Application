3199803b031fd61a209fe457e11e1179ae67f5ba
Buckley <48555@psdschools.org>
2018-12-05 13:32:55 -0700

    Create ClassDao.java

---
+++ app/src/main/java/com/example/a53914/sievemobileapplication/db/ClassDao.java
@@ -0,0 +1,28 @@
+package com.example.a53914.sievemobileapplication.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Defines various ways to interact with the database.
+ */
+
+@Dao
+public interface ClassDao {
+    @Query("SELECT * FROM Class")
+    List<Class> getAll();
+
+    @Insert
+    void insertAll(Class cls);
+
+    @Delete
+    void delete(Class cls);
+
+    @Update
+    void update(Class cls);
+}
