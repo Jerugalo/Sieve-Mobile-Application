b7c0a29724d7506f0e575cb775b4c8d79c9983a8
HFocus <hfocus77@gmail.com>
2019-01-11 14:46:25 -0700

    Run Code Inspection on ClassDatabase

--- app/src/main/java/com/example/a53914/sievemobileapplication/db/ClassDatabase.java
+++ app/src/main/java/com/example/a53914/sievemobileapplication/db/ClassDatabase.java
@@ -1,6 +1,5 @@
 package com.example.a53914.sievemobileapplication.db;
 
-import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
