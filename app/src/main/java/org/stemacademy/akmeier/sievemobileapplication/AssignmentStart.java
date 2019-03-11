6950c41682b9f3be442a5aaefe41cee2db5a3a2c
AKmeier <43421311+AKmeier@users.noreply.github.com>
2019-03-11 13:44:54 -0600

    Minor changes to assignment start XML

--- app/src/main/java/org/stemacademy/akmeier/sievemobileapplication/AssignmentStart.java
+++ app/src/main/java/org/stemacademy/akmeier/sievemobileapplication/AssignmentStart.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
-import android.os.VibrationEffect;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
