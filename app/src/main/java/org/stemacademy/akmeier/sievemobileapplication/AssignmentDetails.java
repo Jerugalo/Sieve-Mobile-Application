ee4bebdee15ce5d8e37771aa262d409236bd8646
AKmeier <43421311+AKmeier@users.noreply.github.com>
2019-04-26 14:30:50 -0600

    fiX!

--- app/src/main/java/org/stemacademy/akmeier/sievemobileapplication/AssignmentDetails.java
+++ app/src/main/java/org/stemacademy/akmeier/sievemobileapplication/AssignmentDetails.java
@@ -281,12 +281,19 @@ public class AssignmentDetails extends AppCompatActivity {
         projectList.clear();
         List<Task> projects = TaskListManager.getProjectList();
         if (projects != null){
-            projectList.add(task.getParentProject());
+            if(task.getParentProject() != null) {
+                projectList.add(task.getParentProject());
+            } else {
+                projectList.add("Select Parent Project");
+            }
+
             for (Task project : projects) {
                 if (!project.getNameID().equals(task.getParentProject())){
                     projectList.add(project.getNameID());
                 }
             }
+        } else {
+            projectList.add(" ");
         }
     }
 
